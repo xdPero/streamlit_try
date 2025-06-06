9d3e1e60af641c83e31b56dc932036313c20ab35
xdPero <77266011+xdPero@users.noreply.github.com>
2025-06-06 20:59:42 +0200

    Updated with a table

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as sl
+import pandas
+
+data={
+    'Series_1' : [1,2,3,4,5],
+    'Series_2' : [10,30,40,50,70]
+}
+
+df = pandas.DataFrame(data)
+
 
 sl.title("My first Streamlit app")
 sl.subheader("Introducing Streamlit")
 sl.write("I hope that you enjoyed my Streamlit app")
+sl.write(df)
